deebd37dd25b2c9ea78fe36ff1b9c8fc3959c73f
mimamulhassankhan <pappuhassan440@gmail.com>
2020-09-28 13:49:24 +0600

    first commit to node mongo

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Thanks for calling me!'));
+app.listen(3000, () => console.log('Connected to server'));
